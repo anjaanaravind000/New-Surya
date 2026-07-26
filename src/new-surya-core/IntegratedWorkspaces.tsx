bac68c97d2c3ca46178d7f991f082486c3cc72aa
anjaanaravind000 <anjaanaravind000@gmail.com>
2026-07-26 11:09:58 +0530

    Add files via upload

--- src/new-surya-core/IntegratedWorkspaces.tsx
+++ src/new-surya-core/IntegratedWorkspaces.tsx
@@ -2,12 +2,12 @@ import type { ComponentType } from 'react';
 import { lazy, Suspense } from 'react';
 import { AuthProvider } from './state/AuthContext';
 import { BakeryStoreProvider } from './state/BakeryStore';
+import KitchenDashboard from './pages/KitchenDashboard';
+import BranchDashboard from './pages/BranchDashboard';
+import BranchInchargeDashboard from './pages/BranchInchargeDashboard';
+import StockAuditDashboard from './pages/StockAuditDashboard';
 
 const AdminDashboard = lazy(() => import('./pages/AdminDashboard'));
-const KitchenDashboard = lazy(() => import('./pages/KitchenDashboard'));
-const BranchDashboard = lazy(() => import('./pages/BranchDashboard'));
-const BranchInchargeDashboard = lazy(() => import('./pages/BranchInchargeDashboard'));
-const StockAuditDashboard = lazy(() => import('./pages/StockAuditDashboard'));
 
 function withCoreProviders(Page: ComponentType) {
   return function IntegratedWorkspace() {
